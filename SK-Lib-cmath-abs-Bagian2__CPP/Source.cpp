0bb123cf76ebcef01ce94d1685034444ccd96b76
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-10 21:11:24 +0700

    Add project files.

---
+++ SK-Lib-cmath-abs-Bagian2__CPP/Source.cpp
@@ -0,0 +1,25 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <conio.h>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	int val1, val2;
+
+	val1 = abs(78);
+	val2 = abs(-194);
+
+	cout << "abs(78) = " << val1 << "\n";
+	cout << "abs(-194) = " << val2 << "\n";
+
+	_getch();
+	return 0;
+}
